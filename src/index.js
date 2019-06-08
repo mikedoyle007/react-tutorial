2ddc73a09aaf179f63c432f18c312d3e1e28f8be
Mike Doyle <mikedoyle007@gmail.com>
2019-06-08 10:55:36 -0700

    Add list of todos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+
+/*
+TODOS
+
+1. Display the location for each move in the format (col, row) in the move history list.
+Bold the currently selected item in the move list.
+
+2. Rewrite Board to use two loops to make the squares instead of hardcoding them.
+
+3. Add a toggle button that lets you sort the moves in either ascending or descending order.
+When someone wins, highlight the three squares that caused the win.
+When no one wins, display a message about the result being a draw.
+*/
+
+
 // class Square extends React.Component {
 //   render() {
 //     return (
